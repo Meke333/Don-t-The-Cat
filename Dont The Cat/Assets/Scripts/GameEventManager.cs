3a0d467e186b188d00e849f43a0c976dbf0aedfb
Mike <mike.li@stud.h-da.de>
2024-01-27 13:16:44 +0100

    HELP

---
+++ Dont The Cat/Assets/Scripts/GameEventManager.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEventManager : MonoBehaviour
+{
+    public GameEventManager Instance;
+
+    #region Events
+
+    public Action onPlayerDied;
+    public Action onWonGame;
+    public Action<double> onCatInteraction;
+    
+
+    #endregion
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    
+    
+}
